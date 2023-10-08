46fa143f8118bf7b71cbf3a48281915a7e2ab33d
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-08 17:27:22 -0500

    Import express. Create port const and app const. Create get request with a "API is running" res. Set up listen func for the port.

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
+import express from "express";
+
+const port = 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
+
 console.log("Hello World!")
