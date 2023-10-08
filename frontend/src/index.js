ba19d1127e3bae8b2ce529d63d2f3c1f74815c82
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-07 20:23:28 -0500

    Create router with createBrowserRouter and CreateRoutesFromElements in index.js. Pass router as props to RouterProvider in index.js. Import and render Outlet in App.js.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,10 +7,20 @@ import "./assets/styles/index.css";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import HomeScreen from './screens/HomeScreen';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+    <Route index={true} path="/" element={<HomeScreen />} />
+    </Route>
+  )
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
