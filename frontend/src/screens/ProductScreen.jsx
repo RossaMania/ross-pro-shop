b3e37f3aa1d28eb4bacfcfb54a106a835656f9ad
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-08 20:18:58 -0500

    Install axios. Set up proxy. Import useState and useEffect in HomeScreen, and use useEffect for fetchProducts func to fetch data. Same with ProductScreen.

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
-import Rating from '../components/Rating';
-import products from "../products"
+import Rating from "../components/Rating";
+import axios from "axios";
 
 const ProductScreen = () => {
 
+  const [product, setProduct] = useState({});
+
   const { id: productId } = useParams();
-  const product = products.find(product => product._id === productId)
+
   console.log(product)
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    }
+
+    fetchProduct();
+  }, [productId])
 
   return (
     <>
