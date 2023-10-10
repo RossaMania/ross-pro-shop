94bacddccbc8fffa90cbe4279b88037f44a635ce
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-10 15:40:55 -0500

    Create middleware folder in backend. Create asyncHandler.js file in middleware folder. Wrap routes  in productRoutes with asyncHandler, import Product model, and create product consts with finding data.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,15 +2,31 @@ import express from "express";
 
 const router = express.Router();
 
-import products from "../data/products.js";
+import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../models/productModel.js";
 
-router.get("/", (req, res) => {
-  res.json(products);
-});
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    if (products) {
+      res.json(products);
+    } else {
+      res.status(404).json({ message: "No products found!" });
+    }
+  })
+);
 
-router.get("/:id", (req, res) => {
-  const product = products.find((product) => product._id === req.params.id);
-  res.json(product);
-});
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  })
+);
 
 export default router;
