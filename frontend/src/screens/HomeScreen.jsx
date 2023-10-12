449aa0da1de1adb4f632e04e99e3a0e07f605234
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-11 21:43:58 -0500

    Create Message func component in components folder. Import and return Alert from react-bootstrap. Style with info default variant. Import and render into HomeScreen and PageScreen components.

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -4,6 +4,7 @@ import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
 import { useGetProductsQuery } from "../slices/productsApiSlice";
 import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 const HomeScreen = () => {
 
@@ -13,7 +14,7 @@ const HomeScreen = () => {
     <>
     { isLoading ? (
       <Loader />
-    ) : error ? (<div>{ error?.data?.message || error.error }</div>) : (<> <h1>Latest Products</h1>
+    ) : error ? (<Message variant="danger">{ error?.data?.message || error.error }</Message>) : (<> <h1>Latest Products</h1>
       <Row>
         {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
