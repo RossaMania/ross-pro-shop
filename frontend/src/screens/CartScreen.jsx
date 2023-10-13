f6a5d30f8c1137857017fc819991e095bb21f822
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-13 15:22:12 -0500

    Render item quantity as a second row.

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -37,9 +37,26 @@ const CartScreen = () => {
                     <Image src={item.image} alt={item.name} fluid rounded />
                   </Col>
                   <Col md={3}>
-                  <Link to={`/product/${item._id}`}>{item.name}</Link>
+                    <Link to={`/product/${item._id}`}>{item.name}</Link>
                   </Col>
                   <Col md={2}>${item.price}</Col>
+                  <Col md={2}></Col>
+                </Row>
+                <Row>
+                  <Col md={2}>Qty</Col>
+                  <Col md={2}>
+                    <Form.Control
+                      as="select"
+                      value={item.qty}
+                      //onChange={(event) => setQty(Number(event.target.value))}
+                    >
+                      {[...Array(item.countInStock).keys()].map((i) => (
+                        <option key={i + 1} value={i + 1}>
+                          {i + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
                 </Row>
               </ListGroup.Item>
             ))}
