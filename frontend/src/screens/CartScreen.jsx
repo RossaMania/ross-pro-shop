0bfba054703df24951d539f9642026d6cf71ed80
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-13 15:05:18 -0500

    Delete placeholder text for items. Map through cartItems array to find item. Render as ListGroup.Item with item image.

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import { Link, useNavigate } from "react-router-dom"
-import { useDispatch, useSelector} from "react-redux";
-import { Row, Col, ListGroup, Image, Form, Button, Card } from "react-bootstrap"
-import { FaTrash } from "react-icons/fa"
-import Message from "../components/Message"
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from "react-bootstrap";
+import { FaTrash } from "react-icons/fa";
+import Message from "../components/Message";
 
 const CartScreen = () => {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
@@ -16,18 +24,26 @@ const CartScreen = () => {
     <Row>
       <Col md={8}>
         <h1 style={{ marginBottom: "20px" }}>Shopping Cart</h1>
-        { cartItems.length === 0 ? (
-          <Message>
+        {cartItems.length === 0 ? (
+          <Message variant="danger">
             Your cart is empty! <Link to="/">Go Back</Link>
           </Message>
         ) : (
           <ListGroup variant="flush">
-          Items Placeholder
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item._id}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
           </ListGroup>
-        ) }
+        )}
       </Col>
     </Row>
-  )
-}
+  );
+};
 
-export default CartScreen
+export default CartScreen;
