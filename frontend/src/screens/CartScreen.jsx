1e7c5ede752e891fd396993dd10f0cbe32270687
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-13 18:20:03 -0500

    Create h3 element with "Grand total" text, and showing the total price of the items in the cart. Use reduce method on cartItems array, and set the accumulator to 0. Change the md size prop of each Col to 6.

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -22,20 +22,20 @@ const CartScreen = () => {
   const { cartItems } = cart;
 
   const addToCartHandler = async (product, qty) => {
-    dispatch(addToCart({...product, qty}))
-  }
+    dispatch(addToCart({ ...product, qty }));
+  };
 
   const removeFromCartHandler = async (id) => {
-    dispatch(removeFromCart(id))
+    dispatch(removeFromCart(id));
   };
 
   const checkOutHandler = () => {
     navigate("/login?redirect=/shipping");
-  }
+  };
 
   return (
     <Row>
-      <Col md={8}>
+      <Col md={6}>
         <h1 style={{ marginBottom: "20px" }}>Shopping Cart</h1>
         {cartItems.length === 0 ? (
           <Message variant="danger">
@@ -46,7 +46,7 @@ const CartScreen = () => {
             {cartItems.map((item) => (
               <ListGroup.Item key={item._id}>
                 <Row>
-                  <Col md={2} style={{marginBottom: "10px"}}>
+                  <Col md={2} style={{ marginBottom: "10px" }}>
                     <Image src={item.image} alt={item.name} fluid rounded />
                   </Col>
                   <Col md={3}>
@@ -59,7 +59,9 @@ const CartScreen = () => {
                     <Form.Control
                       as="select"
                       value={item.qty}
-                      onChange={(event) => addToCartHandler(item, Number(event.target.value))}
+                      onChange={(event) =>
+                        addToCartHandler(item, Number(event.target.value))
+                      }
                     >
                       {[...Array(item.countInStock).keys()].map((i) => (
                         <option key={i + 1} value={i + 1}>
@@ -69,7 +71,11 @@ const CartScreen = () => {
                     </Form.Control>
                   </Col>
                   <Col md={2}>
-                    <Button type="button" variant="light" onClick={() => removeFromCartHandler(item._id)}>
+                    <Button
+                      type="button"
+                      variant="light"
+                      onClick={() => removeFromCartHandler(item._id)}
+                    >
                       <FaTrash />
                     </Button>
                   </Col>
@@ -79,26 +85,63 @@ const CartScreen = () => {
           </ListGroup>
         )}
       </Col>
-      <Col md={4}>
-      <Card>
-        <ListGroup variant="flush">
-        <ListGroup.Item>
-          <h2>
-            Subtotal ({ cartItems.reduce((accumulator, item) => accumulator + item.qty, 0)}) items
-          </h2>
-          <h3>Total before tax:
-            ${cartItems.reduce((accumulator, item) => accumulator + item.qty * item.price, 0).toFixed(2)}
-          </h3>
-          <h4>Estimated tax to be collected:</h4>
-          <h3>${cartItems.reduce((accumulator, item) => accumulator + item.qty * item.price * 0.15, 0).toFixed(2)}</h3>
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <Button type="button" className="btn-block" disabled={cartItems.length === 0} onClick={checkOutHandler}>
-            Proceed to Checkout
-          </Button>
-        </ListGroup.Item>
-        </ListGroup>
-      </Card>
+      <Col md={6}>
+        <Card>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>
+                Subtotal (
+                {cartItems.reduce(
+                  (accumulator, item) => accumulator + item.qty,
+                  0
+                )}
+                ) items
+              </h2>
+              <h3>
+                Total before tax: $
+                {cartItems
+                  .reduce(
+                    (accumulator, item) => accumulator + item.qty * item.price,
+                    0
+                  )
+                  .toFixed(2)}
+              </h3>
+              <h4>Estimated tax to be collected:</h4>
+              <h3>
+              $
+                {cartItems
+                  .reduce(
+                    (accumulator, item) =>
+                      accumulator + item.qty * item.price * 0.15,
+                    0
+                  )
+                  .toFixed(2)}
+              </h3>
+              <h3>
+                Grand total: $
+                {cartItems
+                  .reduce(
+                    (accumulator, item) =>
+                      accumulator +
+                      item.qty * item.price * 0.15 +
+                      (accumulator + item.qty * item.price),
+                    0
+                  )
+                  .toFixed(2)}
+              </h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                type="button"
+                className="btn-block"
+                disabled={cartItems.length === 0}
+                onClick={checkOutHandler}
+              >
+                Proceed to Checkout
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
       </Col>
     </Row>
   );
