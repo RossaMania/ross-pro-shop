522927aa838b3c8866a750aac6fa6a8e8fd94741
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-14 13:48:18 -0500

    Fix file formatting of userRoutes export in userRoutes.js. Fix file formatting of userRoutes import in server.js. Set "/api/users" as userRoutes in Express app. Fix get route for /profile in userRoutes.js.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -18,6 +18,7 @@ import {
 router.route("/").post(registerUser).get(getUsers); //Either a regular user registers or an Admin user gets all users.
 router.post("/logout", logoutUser); //User logs out. This clears the cookie.
 router.post("/login", authUser); //User logs in. This sets the cookie.
-router.route("profile").get(getUserProfile).put(updateUserProfile); //Either a user gets their own user profile page or updates their own user profile page.
+router.route("/profile").get(getUserProfile).put(updateUserProfile); //Either a user gets their own user profile page or updates their own user profile page.
 router.route("/:id").delete(deleteUser).get(getUserByID).put(updateUser); //An Admin user deletes a regular user, Admin user gets user by ID, or Admin user updates user
+
 export default router;
