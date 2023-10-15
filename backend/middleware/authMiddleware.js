015fc09f02ca9810a5fdc400bb5876b3288ed527
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-15 13:08:18 -0500

    Create authMiddleware.js file in middleware folder and import jwt, custom asyncHandler, and User model into authMiddleware.js. Create protect middleware func to read the JWT from the cookie, and use the JWT as the req.user object for any protected routes once the user info in the token is found with the User model.

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+import asyncHandler from "./asyncHandler";
+import User from "../models/userModel";
+
+// Protect routes function
+export const protect = asyncHandler(async (req, res, next) => {
+
+  let token;
+
+  // Read the JWT from the cookie
+
+  token = req.cookies.jwt; //name of the token variable in the res.cookie that was set in userController.js.
+
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET); //decoded const is the payload of the JWT. It becomes the req.user object.
+      req.user = await User.findById(decoded.userId).select("-password"); //We can then use the User model to find the user by ID from the const decoded payload.
+      next(); //We can then use the req.user object in any protected routes. We call the next() function to move on to the next middleware function.
+    } catch (error) {
+      console.log(error);
+       res.status(401); //If there's an error, that means the token is there, but it's not the correct token.
+       throw new Error("Oops! Not authorized, token failed!");
+    }
+
+  } else {
+    res.status(401);
+    throw new Error("Oops! Not authorized, no token!");
+  }
+
+})
+
+
+// Admin middleware function
