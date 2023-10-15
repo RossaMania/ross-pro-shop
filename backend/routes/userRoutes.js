2c5bd721fa181ac43c124cc16d3b2547f794696d
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-15 13:59:28 -0500

    Import protect and admin middleware funcs into userRoutes.js. Add protect func to routes that user needs to be logged in. Add protect and admin funcs to routes for admin users that need to be logged in. Add .js tags to file imports in authMiddleware.js.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -13,12 +13,17 @@ import {
   deleteUser,
   updateUser,
 } from "../controllers/userController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
 // "/" === /api/users
-router.route("/").post(registerUser).get(getUsers); //Either a regular user registers or an Admin user gets all users.
+router.route("/").post(registerUser).get(protect, admin, getUsers); //Either a regular user registers or an Admin user gets all users.
 router.post("/logout", logoutUser); //User logs out. This clears the cookie.
 router.post("/login", authUser); //User logs in. This sets the cookie.
-router.route("/profile").get(getUserProfile).put(updateUserProfile); //Either a user gets their own user profile page or updates their own user profile page.
-router.route("/:id").delete(deleteUser).get(getUserByID).put(updateUser); //An Admin user deletes a regular user, Admin user gets user by ID, or Admin user updates user
+router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile); //Either a user gets their own user profile page or updates their own user profile page.
+router
+  .route("/:id")
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserByID)
+  .put(protect, admin, updateUser); //An Admin user deletes a regular user, Admin user gets user by ID, or Admin user updates user
 
 export default router;
