c6f4d7d707b59ece7971c489fcf236ecdaba8477
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-16 15:16:07 -0500

    Import authSliceReducer into store.js. Add authSliceReducer to the store.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,15 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
 
 import { apiSlice } from "./slices/apiSlice";
 
 import cartSliceReducer from "./slices/cartSlice";
 
+import authSliceReducer from "./slices/authSlice";
+
 const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
-        cart: cartSliceReducer,
+        cart: cartSliceReducer, // Add the cart slice reducer function to the store.
+        auth: authSliceReducer, // Add the auth slice reducer function to the store.
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware), // Add the apiSlice middleware to the store
     devTools: true,
 });
 
