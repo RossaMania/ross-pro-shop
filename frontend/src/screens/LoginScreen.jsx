7369db5fb0686a5d7fc76cdc4ecf66edea0c63f5
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-18 14:31:33 -0500

    Create useEffect hook with userInfo, redirect, and navigate as dependencies. The useEffect will check if there is userInfo in localStorage. If so, user continues on that redirect path. If no userInfo in localStorage, user goes to root.

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -9,9 +9,44 @@ import { setCredentials } from "../slices/authSlice.js";
 import { toast } from "react-toastify";
 
 const LoginScreen = () => {
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch(); // useDispatch hook to dispatch actions
+  const navigate = useNavigate(); // useNavigate hook to navigate programmatically
+
+  // useLoginMutation hook to login.
+  // It is destructured as an array with the login function and a boolean indicating if the request is loading
+  const [login, { isLoading }] = useLoginMutation();
+
+// Destructure userInfo from the auth state by using useSelector hook.
+// The useSelector takes in a function that passes in the state.
+// We want the auth part of the state because that is where the userInfo is.
+  const { userInfo } = useSelector((state) => state.auth);
+
+  // Destructure the search property from the useLocation hook. 
+  const { search } = useLocation();
+
+  // Create a new URLSearchParams object from the search property.
+  // We pass the search property into the urlSearchParams constructor.
+  // This will allow us to get the redirect query parameter from the URL.
+  const searchParams = new URLSearchParams(search);
+
+  // We create a variable named redirect. We want to see if there's this redirect query parameter in the URL.
+  // If there is, we want to use that as the redirect path. If not, we want to use the root path.
+  const redirect = searchParams.get("redirect") || "/";
+
+
+  // useEffect hook to check if the user is logged in.
+  // If the user is logged in, we want to redirect them to the redirect path or the homepage.
+  // We pass in the userInfo and the redirect variable as a dependency.
+  // If the userInfo changes, we want to run this useEffect hook again.
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [userInfo, redirect, navigate] );
 
   const submitHandler = (event) => {
     event.preventDefault();
@@ -47,7 +82,7 @@ const LoginScreen = () => {
       </Form>
       <Row className="py-3">
         <Col>
-        New Customer? <Link to="/register">Register here!</Link>
+          New Customer? <Link to="/register">Register here!</Link>
         </Col>
       </Row>
     </FormContainer>
