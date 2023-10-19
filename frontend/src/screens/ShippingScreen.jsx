50a6d375da7a131cbaec60c690efbcc111a60ff0
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-19 16:01:55 -0500

    Create the shipping form by wrapping Form element with FormContainer, and creating four Form.Groups, one for each piece of state. Create submitHandler func with console log. Wire submitHandler to Form element with onSubmit attribute.

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -8,9 +8,53 @@ const ShippingScreen = () => {
   const [postalCode, setPostalCode] = useState("");
   const [country, setCountry] = useState("");
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log("Submitted!");
+  }
   return (
-    <div>ShippingScreen</div>
-  )
+    <FormContainer>
+      <h1>Shipping</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="address" className="my-2">
+          <Form.Label>Address</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter address"
+            value={address}
+            onChange={(event) => setAddress(event.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId="city" className="my-2">
+          <Form.Label>City</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter city"
+            value={city}
+            onChange={(event) => setCity(event.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId="postalCode" className="my-2">
+          <Form.Label>Postal Code</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter postal code"
+            value={postalCode}
+            onChange={(event) => setPostalCode(event.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId="country" className="my-2">
+          <Form.Label>Country</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter country"
+            value={country}
+            onChange={(event) => setCountry(event.target.value)}
+          />
+        </Form.Group>
+      </Form>
+    </FormContainer>
+  );
 }
 
 export default ShippingScreen
