28f590bf51f5641156b52bcedc38bb6d0df75861
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-19 16:52:10 -0500

    Import useDispatch & useSelector from React Redux. Import useNavigate from react-router-dom. Import saveShippingAddress from cartSlice.js. Get cart state with useSelector, then destructure shipping address from the cart state. Set initial state of address, city, postalCode, & country to either the shipping address in cart state, or to an empty string. Dispatch the saveShippingAddress action in submitHandler so the shipping address changes in the state and saves to localStorage. Then navigate to the payment page.

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -1,17 +1,36 @@
 import React, { useState } from 'react'
 import { Form, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import FormContainer from "../components/FormContainer";
+import { saveShippingAddress } from "../slices/cartSlice.js";
 
 const ShippingScreen = () => {
-  const [address, setAddress] = useState("");
-  const [city, setCity] = useState("");
-  const [postalCode, setPostalCode] = useState("");
-  const [country, setCountry] = useState("");
+  //We need to get the cart state with useSelector.
+  const cart = useSelector((state) => state.cart);
+
+  //We destructure the shipping address from the cart state.
+  const { shippingAddress } = cart;
+
+  //We need to set the initial state of the address, city, postalCode, and country to the shipping address in the cart state.
+  const [address, setAddress] = useState(shippingAddress?.address || "");
+  const [city, setCity] = useState(shippingAddress?.city || "");
+  const [postalCode, setPostalCode] = useState(shippingAddress?.postalCode || "");
+  const [country, setCountry] = useState(shippingAddress?.country || "");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  //Dispatch the saveShippingAddress action so we change the shipping address in the state and save to localStorage.
+  //When we dispatch, we call saveShippingAddress and pass in the address, city, postalCode, and country.
+  //Then we navigate to the payment page.
 
   const submitHandler = (event) => {
     event.preventDefault();
+    dispatch(saveShippingAddress({ address, city, postalCode, country }));
+    navigate("/payment");
     console.log("Submitted!");
-  }
+  };
   return (
     <FormContainer>
       <h1>Shipping</h1>
