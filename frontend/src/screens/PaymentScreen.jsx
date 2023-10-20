0a1d02f4bdc14dc43a1b482784b4ff53d15b73af
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-19 21:56:37 -0500

    Import useEffect from React. Import useDispatch and useSelector from React Redux. Import useNavigate from react-router-dom. Import savePaymentMethod from slices folder. Call useDispatch with dispatch const. Call useNavigate with navigate const. Use useSelector hook to get the cart state. Destructure the shippingAddress from cart state. Use useEffect to conditionally render /payment page while checking localStorage. If there is no shipping address saved in the cart state, it redirects back to /shipping.

--- frontend/src/screens/PaymentScreen.jsx
+++ frontend/src/screens/PaymentScreen.jsx
@@ -1,10 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { Form, Button, Col } from "react-bootstrap";
 import FormContainer from "../components/FormContainer";
 import CheckoutSteps from "../components/CheckoutSteps";
+import { savePaymentMethod } from "../slices/cartSlice.js";
 
 const PaymentScreen = () => {
   const [paymentMethod, setPaymentMethod] = useState("PayPal");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  //We need to get the cart state with useSelector.
+  const cart = useSelector((state) => state.cart);
+
+  //We destructure the shipping address from the cart state.
+  const { shippingAddress } = cart
+
+  useEffect(() => {
+    //If there is no shipping address in the cart state, we redirect to the shipping page so users can enter address.
+    //This is checking localStorage for that shipping address.
+    if (!shippingAddress) {
+      navigate("/shipping");
+    }
+  }, [shippingAddress, navigate]);
 
   return (
     <FormContainer>
