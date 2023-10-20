56aa646e5b8b4c4a70a045821d4c1160d5bb209c
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-20 17:57:33 -0500

    In orderRoutes.js, import express from express, create router const with express.Router() call. Import the order controller funcs. Import the protect and admin from authMiddleware.js. Create routes with router.route. Import orderRoutes into server.js, and set orderRoutes to use /api/orders with app.use.

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,22 @@
+import express from "express";
+
+const router = express.Router();
+
+import {
+  addOrderItems,
+  getOrderById,
+  getMyOrders,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  getOrders,
+} from "../controllers/orderController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
+
+// "/" === /api/orders
+router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders); //Either a regular user creates order or an Admin user gets all the orders.
+router.route("/mine").get(protect, getMyOrders); //A regular user gets their own orders.
+router.route("/:id").get(protect, admin, getOrderById); //An Admin user gets order by ID.
+router.route("/:id/pay").put(protect, updateOrderToPaid); //A regular user updates order to paid.
+router.route("/:id/deliver").put(protect, admin, updateOrderToDelivered); //An Admin user updates order to delivered status.
+
+export default router;
