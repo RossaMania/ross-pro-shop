f9f49ebb7d868c8ab6b22a05ede498dcf90f6e0e
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-20 16:38:21 -0500

    Create orderController.js in backend/controllers. Create orderRoutes.js in backend/routes. Create controller func routes in the orderControllers.js file, and export.

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,47 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+
+import Order from "../models/orderModel.js";
+
+// @desc    Create new order
+// @route   POST /api/orders
+// @access  Private
+const addOrderItems = asyncHandler(async (req, res) => {
+  res.send("Add order items!");
+});
+
+// @desc    Get logged in user's orders.
+// @route   GET /api/orders/:id
+// @access  Private
+const getOrderById = asyncHandler(async (req, res) => {
+  res.send("Get my orders!");
+});
+
+// @desc    Get order by ID.
+// @route   GET /api/orders/myorders
+// @access  Private
+const getMyOrders = asyncHandler(async (req, res) => {
+  res.send("Get order by ID!");
+});
+
+// @desc    Update order to paid. This is an admin user route.
+// @route   GET /api/orders/:id/pay
+// @access  Private/Admin
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  res.send("Update order to paid!");
+});
+
+// @desc    Update order to delivered. This is an admin user route.
+// @route   GET /api/orders/:id/deliver
+// @access  Private/Admin
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  res.send("Update order to delivered!");
+});
+
+// @desc    Update order to delivered. This is an admin user route.
+// @route   GET /api/orders/:id/deliver
+// @access  Private/Admin
+const getOrders = asyncHandler(async (req, res) => {
+  res.send("Get all orders!");
+});
+
+export { addOrderItems, getOrderById, getMyOrders, updateOrderToPaid, updateOrderToDelivered, getOrders };
