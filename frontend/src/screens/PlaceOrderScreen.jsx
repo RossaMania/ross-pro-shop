f97ffa8d87b34474070bff0ac5b412f203ba1f84
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-20 21:53:03 -0500

    Fix bug by changing shippingAddress.address to shippingAddress?.address in PlaceOrderScreen. Add a row with two columns in return, with placeholder text "Column!"

--- frontend/src/screens/PlaceOrderScreen.jsx
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -14,14 +14,18 @@ const PlaceOrderScreen = () => {
   useEffect(() => {
     // If there is no address in the shippingAddress object of the cart state, then navigate to the shipping screen.
     // If there is no payment method in the paymentMethod object of the cart state, then navigate to the payment screen.
-    if (!cart.shippingAddress.address) {
+    if (!cart.shippingAddress?.address) {
       navigate("/shipping")
     } else if (!cart.paymentMethod) {
       navigate("/payment")
     }
-  }, [cart.shippingAddress.address, cart.paymentMethod, navigate]);
+  }, [cart.shippingAddress?.address, cart.paymentMethod, navigate]);
   return <div>
     <CheckoutSteps step1 step2 step3 step4 />
+    <Row>
+      <Col md={8}>Column!</Col>
+      <Col md={4}>Column!</Col>
+    </Row>
   </div>;
 };
 
