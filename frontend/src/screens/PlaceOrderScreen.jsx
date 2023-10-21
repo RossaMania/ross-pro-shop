9e8172a045702bd814691a668aa24e84810a5f8d
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-21 14:04:46 -0500

    Set a ListGroup.Item to show Payment Method. Set another ListGroup to conditionally render the order items by mapping through them. If there is no items in orderItems array show "Your cart is empty!" message. Render two columns in the list: one shows the item info, other column shows item quantity and item price.

--- frontend/src/screens/PlaceOrderScreen.jsx
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -34,12 +34,43 @@ const PlaceOrderScreen = () => {
             <ListGroup.Item>
               <h2>Shipping</h2>
               <p>
-                <strong>Address:</strong>
+                <strong>Address: </strong>
                 {cart.shippingAddress?.address}, {cart.shippingAddress?.city},{" "}
                 {cart.shippingAddress?.postalCode},{" "}
                 {cart.shippingAddress?.country}
               </p>
             </ListGroup.Item>
+            <ListGroup.Item>
+              <h2>Payment Method</h2>
+              <strong>Method: </strong>
+              {cart?.paymentMethod}
+            </ListGroup.Item>
+            <ListGroup.Item>
+            <h2>Order Items</h2>
+            {cart.cartItems.length === 0 ? (
+              <Message>Your cart is empty!</Message>
+            ) : (
+              <ListGroup variant="flush">
+                {cart.cartItems.map((item, index) => (
+                  <ListGroup.Item key={index}>
+                  <Row>
+                    <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                    </Col>
+                    <Col>
+                      <Link to={`/products/${item.product}`}>
+                        {item.name}
+                      </Link>
+                    </Col>
+                    <Col md={4}>
+                    {item.qty} x ${item.price} = ${item.qty * item.price}
+                    </Col>
+                  </Row>
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            )}
+          </ListGroup.Item>
           </ListGroup>
         </Col>
         <Col md={4}>Column!</Col>
