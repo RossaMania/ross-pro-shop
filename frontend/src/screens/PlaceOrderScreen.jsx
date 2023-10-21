104f178ee1251b35f28ad0b2f7de7e81354bbe85
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-20 21:49:03 -0500

    Import useEffect from React, Link & useNavigate from react-router-dom, useSelector from React Redux, and stylings from react-bootstrap. Import CheckoutSteps component. Set up navigate func with useNavigate hook. Use useSelector hook to select the cart state. Use useEffect so if there is no address in the shippingAddress object of the cart state, then navigate to /shipping. Else if there is no payment method in the paymentMethod object of the cart state, then navigate to /payment. Set the cart.shippingAddress.address, cart.paymentMethod, navigate as dependencies. Return CheckoutSteps component with step1, step2, step3, step4 as props.

--- frontend/src/screens/PlaceOrderScreen.jsx
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Col, Row, ListGroup, Image, Card, Button } from "react-bootstrap";
+import CheckoutSteps from "../components/CheckoutSteps";
 
 const PlaceOrderScreen = () => {
-  return (
-    <div>PlaceOrderScreen</div>
-  )
-}
+  // useNavigate() is a hook that returns the navigate function which takes a string as an argument and navigates to the path.
+  const navigate = useNavigate();
 
-export default PlaceOrderScreen
+  // Use the useSelector hook to select the cart state.
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    // If there is no address in the shippingAddress object of the cart state, then navigate to the shipping screen.
+    // If there is no payment method in the paymentMethod object of the cart state, then navigate to the payment screen.
+    if (!cart.shippingAddress.address) {
+      navigate("/shipping")
+    } else if (!cart.paymentMethod) {
+      navigate("/payment")
+    }
+  }, [cart.shippingAddress.address, cart.paymentMethod, navigate]);
+  return <div>
+    <CheckoutSteps step1 step2 step3 step4 />
+  </div>;
+};
+
+export default PlaceOrderScreen;
