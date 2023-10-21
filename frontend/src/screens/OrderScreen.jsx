c9babb22daa7a68ca0baacf28b7698dcb19ec132
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-21 17:01:18 -0500

    Create OrderScreen func component in screens folder. Import Link & useParams from react-router-dom, import stylings from react-bootstrap, import Message & Loader from components folder, and import useGetOrderDetailsQuery hook from slices folder. Import OrderScreen into index.js, and set up Route in PrivateRoute child config with /order/:id path, and OrderScreen element, all in the createRoutesFromElements config.

---
+++ frontend/src/screens/OrderScreen.jsx
@@ -0,0 +1,21 @@
+import { Link, useParams } from "react-router-dom";
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from "react-bootstrap";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { useGetOrderDetailsQuery } from "../slices/ordersApiSlice";
+
+import React from "react";
+
+const OrderScreen = () => {
+  return <div>OrderScreen</div>;
+};
+
+export default OrderScreen;
