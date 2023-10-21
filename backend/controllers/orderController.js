2ed7ba548b55001076dd5d9488c7f87ce42a4ae5
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-20 20:08:25 -0500

    Remove response in addOrderItems controller func, and destructure req.body from the HTTP request, then check for an orderItems array. If there is one, & it's empty, we throw an error. Else, create an order with the order const. Map through the orderItems array & return new array of items to add the product ID to orderItems array, get the user ID from the token, then save the order as createOrder by calling save() on order const. Send a 201 status with JSON data.

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,39 @@ import Order from "../models/orderModel.js";
 // @route   POST /api/orders
 // @access  Private
 const addOrderItems = asyncHandler(async (req, res) => {
-  res.send("Add order items!");
+  const { orderItems, shippingAddress, paymentMethod, itemsPrice, taxPrice, shippingPrice, totalPrice, } = req.body; //Destructure req.body from the HTTP request.
+
+//Check for an order items array. If there is an order items array, we want to see if it's empty or not.
+if (orderItems && orderItems.length) {
+  res.status(400); //Bad request
+  throw new Error("Oops! No items in this order!"); //Throw an error if there are no order items.
+} else {
+  //If there are order items, create a new order.
+  //This just creates an order and puts it in the order const. Nothing is saved in the database.
+  const order = new Order({
+    orderItems: orderItems.map((item) => ({
+      //Map through the order items array and return a new array of order items.
+      //This is because we want to add the product ID to the order items array.
+      ...item,
+      product: item._id,
+      //Remove the _id field from the order items array.
+      //This is because there's no _id field in the orderModel.js orderItems array.
+      _id: undefined,
+    })),
+    user: req.user._id, //Get the user ID from the token.
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  });
+
+  const createOrder = await order.save(); //Save the order to the database.
+
+  res.status(201).json(createOrder); //Send a 201 status code (something was created) and send the order data in JSON format.
+}
+
 });
 
 // @desc    Get logged in user's orders.
