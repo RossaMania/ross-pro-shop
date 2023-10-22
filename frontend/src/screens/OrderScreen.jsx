58b9c80b54dc49634a7ac536e05586637d834245
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-22 13:55:07 -0500

    Set a fragment in the OrderScreen return with a Row and two Columns. Set first column to show a ListGroup and a ListGroup.Item with the shipping information of order.

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -33,7 +33,16 @@ const OrderScreen = () => {
     <>
       <h1>Order {order._id}</h1>
       <Row>
-        <Col md={8}>Column</Col>
+        <Col md={8}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>Shipping</h2>
+              <p><strong>Name: </strong>{order.user.name}</p>
+              <p><strong>E-mail: </strong>{order.user.email}</p>
+              <p><strong>Address: </strong>{order.shippingAddress.address}, {order.shippingAddress.city} {order.shippingAddress.postalCode}, {order.shippingAddress.country}</p>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
         <Col md={4}>Column</Col>
       </Row>
     </>
