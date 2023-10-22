e571cff6eb0bd8114571d2ebcafb33e5dc73e735
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-22 15:20:45 -0500

    Render a Card in the second column with the Order Summary--Total cost of the items, total shipping cost, total tax cost, and the grand total of the items, shipping, and tax. Comment in button placeholders for payment and marking order as delivered. Add code comments.

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -25,6 +25,8 @@ const OrderScreen = () => {
     error,
   } = useGetOrderDetailsQuery(orderId);
 
+  // First, if it's loading, then we show the Loader component. If there's an error, we show the Message component.
+  //If there's no error and it's not loading, then we show the order details.
   return isLoading ? (
     <Loader />
   ) : error ? (
@@ -37,11 +39,25 @@ const OrderScreen = () => {
           <ListGroup variant="flush">
             <ListGroup.Item>
               <h2>Shipping</h2>
-              <p><strong>Name: </strong>{order.user.name}</p>
-              <p><strong>E-mail: </strong>{order.user.email}</p>
-              <p><strong>Address: </strong>{order.shippingAddress.address}, {order.shippingAddress.city} {order.shippingAddress.postalCode}, {order.shippingAddress.country}</p>
+              <p>
+                <strong>Name: </strong>
+                {order.user.name}
+              </p>
+              <p>
+                <strong>E-mail: </strong>
+                {order.user.email}
+              </p>
+              <p>
+                <strong>Address: </strong>
+                {order.shippingAddress.address}, {order.shippingAddress.city}{" "}
+                {order.shippingAddress.postalCode},{" "}
+                {order.shippingAddress.country}
+              </p>
+              {/* Conditionally show if the order has been delivered or not. If delivered, the date is shown. If not, "Not delivered!" is shown.*/}
               {order.isDelivered ? (
-                <Message variant="success">Delivered on {order.deliveredAt}</Message>
+                <Message variant="success">
+                  Delivered on {order.deliveredAt}
+                </Message>
               ) : (
                 <Message variant="danger">Not delivered!</Message>
               )}
@@ -52,6 +68,7 @@ const OrderScreen = () => {
                 <strong>Method: </strong>
                 {order.paymentMethod}
               </p>
+              {/* Conditionally show if the order has been paid for or not. If it has been paid, we show the date it was paid. If not, we show a "Not paid!" message. */}
               {order.isPaid ? (
                 <Message variant="success">Paid on {order.paidAt}</Message>
               ) : (
@@ -62,25 +79,55 @@ const OrderScreen = () => {
               <h2>Order Items</h2>
               {order.orderItems.map((item, index) => (
                 <ListGroup.Item key={index}>
-                <Row>
-                  <Col md={2}>
-                  <Image src={item.image} alt={item.name} fluid rounded />
-                  </Col>
-                  <Col>
-                  <Link to={`/product/${item.product}`}>
-                    {item.name}
-                    </Link>
-                  </Col>
-                  <Col md={4}>
-                  {item.qty} x ${item.price} = ${item.qty * item.price}
-                  </Col>
-                </Row>
+                  <Row>
+                    <Col md={2}>
+                      <Image src={item.image} alt={item.name} fluid rounded />
+                    </Col>
+                    <Col>
+                      <Link to={`/product/${item.product}`}>{item.name}</Link>
+                    </Col>
+                    <Col md={4}>
+                      {item.qty} x ${item.price} = ${item.qty * item.price}
+                    </Col>
+                  </Row>
                 </ListGroup.Item>
               ))}
             </ListGroup.Item>
           </ListGroup>
         </Col>
-        <Col md={4}>Column</Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup>
+              <ListGroup.Item>
+                <h2>Order Summary</h2>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                {/* This shows the price of the total number of items in the cart. */}
+                <Row>
+                  <Col>Items:</Col>
+                  <Col>${order.itemsPrice}</Col>
+                </Row>
+                {/* This shows the price of shipping. */}
+                <Row>
+                  <Col>Shipping:</Col>
+                  <Col>${order.shippingPrice}</Col>
+                </Row>
+                {/* This shows the price of tax. */}
+                <Row>
+                  <Col>Tax:</Col>
+                  <Col>${order.taxPrice}</Col>
+                </Row>
+                {/* This shows the sum of the price of the total number of items in the cart, the shipping price, and the tax price. */}
+                <Row>
+                  <Col>Grand Total:</Col>
+                  <Col>${order.totalPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              {/* PAY ORDER PLACEHOLDER */}
+              {/* MARK AS DELIVERED PLACEHOLDER */}
+            </ListGroup>
+          </Card>
+        </Col>
       </Row>
     </>
   );
