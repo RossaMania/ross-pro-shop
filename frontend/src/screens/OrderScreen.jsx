759cd09288d2c3c620fbec5813acb95084c36a62
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-22 11:30:03 -0500

    Get the id from the URL by destructuring the id into orderId with the useParams hook. Pass the orderId to the useGetOrderDetailsQuery and get the data from the URL & rename the data to order. Destructure the isLoading & isError states, and the refetch method. Console log order.

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Link, useParams } from "react-router-dom";
 import {
   Row,
@@ -12,9 +13,20 @@ import Message from "../components/Message";
 import Loader from "../components/Loader";
 import { useGetOrderDetailsQuery } from "../slices/ordersApiSlice";
 
-import React from "react";
-
 const OrderScreen = () => {
+  const { id: orderId } = useParams(); // Get the id from the url.
+
+  //Use the useGetOrderDetailsQuery hook to fetch the order details.
+  //Pass in the orderId as an argument. Destructure the data, refetch, isLoading, and isError properties from the hook result.
+  const {
+    data: order,
+    refetch,
+    isLoading,
+    isError,
+  } = useGetOrderDetailsQuery(orderId);
+
+  console.log(order);
+
   return <div>OrderScreen</div>;
 };
 
