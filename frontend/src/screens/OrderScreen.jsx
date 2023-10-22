eb56d4543f12e64d876416f80f7c72a973291ac1
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-22 14:45:50 -0500

    Conditionally render whether an order is successfully paid or not. If paid, message appears with the date the order was paid. If not, a "Not paid!" message is rendered.

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -46,6 +46,18 @@ const OrderScreen = () => {
                 <Message variant="danger">Not delivered!</Message>
               )}
             </ListGroup.Item>
+            <ListGroup.Item>
+              <h2>Payment Method</h2>
+              <p>
+                <strong>Method: </strong>
+                {order.paymentMethod}
+              </p>
+              {order.isPaid ? (
+                <Message variant="success">Paid on {order.paidAt}</Message>
+              ) : (
+                <Message variant="danger">Not paid!</Message>
+              )}
+            </ListGroup.Item>
           </ListGroup>
         </Col>
         <Col md={4}>Column</Col>
