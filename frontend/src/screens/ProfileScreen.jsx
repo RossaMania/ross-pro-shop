412e4e21bbbb32c8cab34ebcd47bef1cb3a2a2ff
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-23 16:35:51 -0500

    Set one row and two columns in the return. Set Form with a Form.Group for name, email, password, and confirm password. Create Button under last Form.Group with type submit, and text "Update{". "Column" placeholder text in second column.

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -24,7 +24,7 @@ const ProfileScreen = () => {
       setName(userInfo.name);
       setEmail(userInfo.email);
     }
-  }, [userInfo.name, userInfo.email]);
+  }, [userInfo]);
 
   const submitHandler = (event) => {
     event.preventDefault();
@@ -34,8 +34,54 @@ const ProfileScreen = () => {
 
 
   return (
-    <div>ProfileScreen</div>
-  )
+    <Row>
+      <Col md={3}>
+        <h2>User Profile</h2>
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="name" className="my-2">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="name"
+              placeholder="Enter name"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="email" className="my-2">
+            <Form.Label>E-mail Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter e-mail"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="password" className="my-2">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="confirmPassword" className="my-2">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Confirm password"
+              value={confirmPassword}
+              onChange={(event) => setConfirmPassword(event.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Button type="submit" variant="primary" className="my-2">
+            Update
+          </Button>
+        </Form>
+      </Col>
+      <Col md={9}>Column</Col>
+    </Row>
+  );
 }
 
 export default ProfileScreen
