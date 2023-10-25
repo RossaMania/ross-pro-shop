12a7472496924b293b037678cf3b449128a3cc61
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-25 18:45:21 -0500

    Set table body to map out the products and render the id, name, price, category, and brand. Set a Button in a LinkContainer to lead to edit page. Button consists of FaEdit icon.

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -14,34 +14,58 @@ const ProductListScreen = () => {
 
   console.log(products);
 
-  return <>
-    <Row className="align-items-center">
-      <Col>
-      <h1>Products</h1>
-      </Col>
-      <Col className="text-end">
-      <Button className="btn-sm p-1 m-3">
-      <FaEdit /> Create Product
-      </Button>
-      </Col>
-    </Row>
-    { isLoading ? <Loader /> : error ? <Message variant="danger">{error}</Message> : (
-      <>
-        <Table hover responsive className="table-sm">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>NAME</th>
-            <th>PRICE</th>
-            <th>CATEGORY</th>
-            <th>BRAND</th>
-            <th></th>
-          </tr>
-        </thead>
-        </Table>
-      </>
-    )}
-  </>
+  return (
+    <>
+      <Row className="align-items-center">
+        <Col>
+          <h1>Products</h1>
+        </Col>
+        <Col className="text-end">
+          <Button className="btn-sm p-1 m-3">
+            <FaEdit /> Create Product
+          </Button>
+        </Col>
+      </Row>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <>
+          <Table hover responsive className="table-sm">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>PRICE</th>
+                <th>CATEGORY</th>
+                <th>BRAND</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <tr key={product._id}>
+                  <td>{product._id}</td>
+                  <td>{product.name}</td>
+                  <td>${product.price}</td>
+                  <td>{product.category}</td>
+                  <td>{product.brand}</td>
+                  <td>
+                  <LinkContainer to={`/admin/product/${product._id}/edit`}>
+                    <Button variant="light" className="btn-sm mx-2">
+                    <FaEdit />
+                    </Button>
+                  </LinkContainer>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </>
+      )}
+    </>
+  );
 
   }
 
