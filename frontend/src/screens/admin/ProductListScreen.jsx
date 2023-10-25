de6b44dedbdb66c0c587e6566dc705b60a315aba
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-25 16:49:30 -0500

    Import stylings from react-router-bootstrap and react-bootstrap. Import FaTimes, FaEdit, and FaTrash from react-icons. Import Message & Loader from components folder. Import useGetProductsQuery from /slices/productsApiSlice.js. Destructure the data, isLoading state, and error state from the useGetProductsQuery hook call. Name data as products. Console log products.

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -1,6 +1,18 @@
+import { LinkContainer } from "react-router-bootstrap";
+import { Table, Button, Row, Col } from "react-bootstrap";
+import { FaTimes, FaEdit, FaTrash } from "react-icons/fa";
+import Message from "../../components/Message";
+import Loader from "../../components/Loader";
+import { useGetProductsQuery } from "../../slices/productsApiSlice.js";
+
 import React from 'react'
 
 const ProductListScreen = () => {
+
+  const { data: products, isLoading, error } = useGetProductsQuery(); //Destructure the data, isLoading state, and error state from the useGetProductsQuery hook call.
+
+  console.log(products)
+
   return (
     <div>ProductListScreen</div>
   )
