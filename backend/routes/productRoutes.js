5054e4e661efab987a35d1c192adbea93611cc62
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-26 14:37:57 -0500

    Add put request to productsRoute.js in the router.route("/:id" route. The protect middleware, admin middleware, and updateProduct func are passed in the put.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,6 +7,6 @@ import { getProducts, getProductById, createProduct } from "../controllers/produ
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
-router.route("/:id").get(getProductById);
+router.route("/:id").get(getProductById).put(protect, admin, updateProducts);
 
 export default router;
