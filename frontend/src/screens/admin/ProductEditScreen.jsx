64d3630e77785da0a5bd06263ea0fa7410ac7450
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-26 20:21:13 -0500

    In ProductEditScreen, import useState & useEffect from React, import Link, useNavigate & useParams from react-router-dom, import stylings from react-bootstrap. Import Message, Loader, & FormContainer from components. Set state variables and state funcs with useState hook for name, price, image, brand, category, countInStock, & description. Set useParams hook with destructured id: productId object const. Destructure the data into product, the isLoading state, refetch func, and error state with the useGetProductDetailsQuery. Pass in productId.

--- frontend/src/screens/admin/ProductEditScreen.jsx
+++ frontend/src/screens/admin/ProductEditScreen.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import Message from "../../components/Message";
+import Loader from "../../components/Loader";
+import FormContainer from "../../components/FormContainer";
+import { toast } from "react-toastify";
+import { useUpdateProductMutation, useGetProductDetailsQuery } from "../../slices/productsApiSlice.js";
 
 const ProductEditScreen = () => {
+
+  const { id: productId } = useParams();
+
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [image, setImage] = useState("");
+  const [brand, setBrand] = useState("");
+  const [category, setCategory] = useState("");
+  const [countInStock, setCountInStock] = useState(0);
+  const [description, setDescription] = useState("");
+
+  const { data: product, isLoading, refetch, error } = useGetProductDetailsQuery(productId);
+
+  console.log(product)
+
   return (
     <div>ProductEditScreen</div>
   )
