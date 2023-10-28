51c7fe077a26e0aa90dfd86b06a71e990fc20241
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-28 15:16:08 -0500

    Create deleteProduct func controller in productController.js. Find the product by ID with params.id. If there's a product, use the deleteOne method on the matching product._id, and send a 200 status to client with "Yay! Product deleted!" message. Else, send status 400 with error message.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -72,4 +72,21 @@ const updateProduct = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById, createProduct, updateProduct };
+
+// @desc    Delete a product
+// @route   DELETE /api/products/:id
+// @access  Private/Admin
+const deleteProduct = asyncHandler(async (req, res) => {
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+   await Product.deleteOne({_id: product._id})
+   res.status(200).json({ message: "Yay! Product deleted!"});
+  } else {
+    res.status(404); //That means the product is not found.
+    throw new Error("Oops! Product not found!");
+  }
+});
+
+export { getProducts, getProductById, createProduct, updateProduct, deleteProduct };
