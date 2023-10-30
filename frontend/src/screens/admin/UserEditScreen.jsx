771f3a97b51928548b8fbec83c8bcde4edbfbcee
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-29 22:03:36 -0500

    Import useState & useEffect from React. Import Link, useNavigate, & useParams from react-router-dom. Import stylings from react-bootstrap. Import Message, FormContainer & Loader from components folder. Import toast from react-toastify. Import useGetUserQuery, and useUpdateUserMutation from slices/usersApiSlice. Copy/paste ProductEditScreen code into UserEditScreen & refactor. Set submitHandler to just a console log with "Submitted!"

--- frontend/src/screens/admin/UserEditScreen.jsx
+++ frontend/src/screens/admin/UserEditScreen.jsx
@@ -1,9 +1,102 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import Message from "../../components/Message";
+import Loader from "../../components/Loader";
+import FormContainer from "../../components/FormContainer";
+import { toast } from "react-toastify";
+import {
+  useGetUserDetailsQuery,
+  useUpdateUserMutation,
+} from "../../slices/usersApiSlice";
 
 const UserEditScreen = () => {
-  return (
-    <div>UserEditScreen</div>
-  )
-}
+  const { id: userId } = useParams(); //Get the user id from the URL.
 
-export default UserEditScreen
+  const [name, setName] = useState(""); //Set the name state.
+  const [email, setEmail] = useState(""); //Set the email state.
+  const [isAdmin, setIsAdmin] = useState(false); //Set the isAdmin state.
+
+  const {
+    data: user,
+    isLoading,
+    refetch,
+    error,
+  } = useGetUserDetailsQuery(userId); //Get the user details from the database using the useGetUserDetailsQuery hook call.
+
+  console.log(user);
+
+  //Use the useUpdateUserMutation hook to update the user details in the database.
+  const [updateUser, { isLoading: loadingUpdate }] = useUpdateUserMutation();
+
+  const navigate = useNavigate(); //Use the useNavigate hook to navigate to a different route.
+
+  //Use the useEffect hook to set the name, email, and isAdmin states to the user details from the database when the component loads.
+  useEffect(() => {
+    if (user) {
+      setName(user.name);
+      setEmail(user.email);
+      setIsAdmin(user.isAdmin);
+    }
+  }, [user]); //Only run this useEffect hook when the user changes in the dependency array.
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    console.log("Submitted!");
+  };
+
+  return (
+    <>
+      <Link to="/admin/userlist" className="btn btn-light my-3">
+        Go Back
+      </Link>
+      <FormContainer>
+        <h1>Edit User</h1>
+        {loadingUpdate && <Loader />}
+
+        {isLoading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">{error}</Message>
+        ) : (
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId="name" className="my-2">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter name"
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="email" className="my-2">
+              <Form.Label>E-mail</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter e-mail"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="isAdmin" className="my-2">
+              <Form.Check
+                type="checkbox"
+                label="Is Admin"
+                checked={isAdmin}
+                onChange={(event) => setIsAdmin(event.target.checked)}
+              ></Form.Check>
+            </Form.Group>
+
+            <Button type="submit" variant="primary" className="my-2">
+              Update
+            </Button>
+          </Form>
+        )}
+      </FormContainer>
+    </>
+  );
+};
+
+export default UserEditScreen;
