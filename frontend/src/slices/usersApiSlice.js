acff9c36fb3c327a8578af0ef4eec74afe2ea348
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-29 20:57:11 -0500

    Create two endpoint queries: getUserDetails, and updateUser. getUserDetails is a query object that uses the USERS_URL const with the userId passed in the URL as an argument. This uses the GET method. updateUser is a mutation object that passes in a data object as an argument. The method is PUT since we're updating data, the URL is USERS_URL const with data.userId, and data is sent in the body.

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -37,14 +37,29 @@ export const usersApiSlice = apiSlice.injectEndpoints({
         url: USERS_URL,
         method: "GET",
       }),
-      providesTags: ["Users"], //This is the tag for the data that we are fetching from the backend API endpoint at USERS_URL.
+      providesTags: ["User"], //This is the tag for the data that we are fetching from the backend API endpoint at USERS_URL.
       keepUnusedDataFor: 5, //This is the number of seconds to keep the data in the cache. If the data is not used within this time, then it will be removed from the cache.
     }),
     deleteUser: builder.mutation({
       query: (userId) => ({
-        url: `${USERS_URL}/${userId}`,
-        method: "DELETE",
+        url: `${USERS_URL}/${userId}`, //The url is the USERS_URL + the userId that we pass in as an argument to the query function.
+        method: "DELETE", //The method is DELETE because we are deleting the user by id.
       }),
+    }),
+    getUserDetails: builder.query({
+      query: (userId) => ({
+        url: `${USERS_URL}/${userId}`, //The url is the USERS_URL + the userId that we pass in as an argument to the query function.
+        method: "GET", //The method is GET because we are getting the user details.
+      }),
+      keepUnusedDataFor: 5,
+    }),
+    updateUser: builder.mutation({
+      query: ({ data }) => ({
+        url: `${USERS_URL}/${data.userId}`, //The url is the USERS_URL + the userId that we pass in as an argument to the query function.
+        method: "PUT", //The method is PUT because we are updating the user details.
+        body: data, //Send the data in the body.
+      }),
+      invalidatesTags: ["User"], //This is the tag for the data that we are fetching from the backend API endpoint at USERS_URL.
     }),
   }),
 });
@@ -56,4 +71,6 @@ export const {
   useProfileMutation,
   useGetUsersQuery,
   useDeleteUserMutation,
+  useGetUserDetailsQuery,
+  useUpdateUserMutation,
 } = usersApiSlice;
