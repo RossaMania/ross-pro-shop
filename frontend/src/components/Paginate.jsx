2a7fe5561bb37e5cb5cb8516eed8c6c9b739cc4b
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-31 17:22:42 -0500

    Pass keyword as an empty string in Paginate. Conditionally render /search/${keyword}/page/${i + 1} URL if keyword exists. Refactor code from page to i for index in mapping of Array keys.

--- frontend/src/components/Paginate.jsx
+++ frontend/src/components/Paginate.jsx
@@ -3,22 +3,22 @@ import { LinkContainer } from "react-router-bootstrap";
 
 import React from 'react'
 
-const Paginate = ({ pages, page, isAdmin = false }) => {
+const Paginate = ({ pages, page, isAdmin = false, keyword = "" }) => {
   return (
     //If there is more than one page, then show the Paginate component.
     pages > 1 && (
       //Create an array of page numbers and map through the array to create a pagination item for each page.
       <Pagination>
-        {[...Array(pages).keys()].map((page) => (
+        {[...Array(pages).keys()].map((i) => (
           <LinkContainer //Wrap each pagination item in a LinkContainer component to make each pagination item a link.
-            key={page + 1}
+            key={i + 1}
             //If the user is not an admin, then link to the page number. If the user is an admin, then link to the admin product list page number.
             to={
-              !isAdmin ? `/page/${page + 1}` : `/admin/productlist/${page + 1}`
+              !isAdmin ? keyword ? `/search/${keyword}/page/${i + 1}` : `/page/${i + 1}` : `/admin/productlist/${i + 1}`
             }
           >
-            <Pagination.Item active={page + 1 === page}>
-              {page + 1}
+            <Pagination.Item active={i + 1 === page}>
+              {i + 1}
             </Pagination.Item>
           </LinkContainer>
         ))}
