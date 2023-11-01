2122d187d9171c999ed070a3dfb1fb554a7954b5
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-31 19:46:55 -0500

    Create Button with Search text. Set type, variant, and style.

--- frontend/src/components/SearchBox.jsx
+++ frontend/src/components/SearchBox.jsx
@@ -21,6 +21,9 @@ const SearchBox = () => {
     placeholder="Search products..."
     className="mr-sm-2 ml-sm-5"
     ></Form.Control>
+    <Button type="submit" variant="outline-success" className="p-2 mx-2">
+      Search
+    </Button>
     </Form>
   )
 }
