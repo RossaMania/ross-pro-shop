8334aeccd2d8c669db786681ddaf55d1c87f2b49
Ross Clettenberg <raclettenberg@gmail.com>
2023-10-31 19:08:42 -0500

    Create SearchBox component func in components folder. Import useState from React. Import stylings from react-bootstrap. Import useParams & useNavigate from react-router-dom. Create navigate func with useNavigate hook call. Get keyword object from useParams hook call. Rename keyword to urlKeyword. Create keyword state variable & setKeyword state func with useState hook. urlKeyword or empty string is default state.

---
+++ frontend/src/components/SearchBox.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import { Form, Button } from "react-bootstrap";
+import { useParams, useNavigate } from "react-router-dom";
+
+const SearchBox = () => {
+  const navigate = useNavigate();
+  const { keyword: urlKeyword } = useParams();
+  const [keyword, setKeyword] = useState(urlKeyword || "");
+  return (
+    <div>SearchBox</div>
+  )
+}
+
+export default SearchBox
