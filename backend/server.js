bcc4a29a2e9abeb8ac9146e68828963dc8069bbf
Ross Clettenberg <raclettenberg@gmail.com>
2023-11-02 12:40:20 -0500

    In server.js, set up if check to test if the app is running in production, or in development. If app is running in production, set a static folder at frontend/build and redirect routes to index.htlm. Else, set up the API routes on the dev server.

--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,7 @@ app.use(express.urlencoded({ extended: true })) //Allows us to accept form data 
 //Cookie parser middleware
 app.use(cookieParser());
 
-//Routes
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -39,6 +36,25 @@ app.use("/api/upload", uploadRoutes);
 app.get("/api/config/paypal", (req, res) => res.send({ clientId: process.env.PAYPAL_CLIENT_ID }));
 
 const __dirname = path.resolve(); // Set __dirname to the current directory name.
+
+// We want the server to load the production server or the build server when in production.
+// A test to see if we're in production. If we're in production, then we want to set a static folder.
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static(path.join(__dirname, "/frontend/build")))
+
+  // any route that is not api routes will be redirected to index.html
+  app.get("*", (req, res) =>
+  // load the index.html file that's in the frontend/build folder which we just made static.
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  //If we're not in production, then run the following code to set up our API routes on the dev server.
+  //Routes
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 // Make the uploads folder static so that we can access the images inside it.
 // Pass in uploads to app.use, make it static with express.static, then pass in location of that folder, /uploads, with path.join.
